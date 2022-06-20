7ee2368015c112b344da5e2d15b3a9c783000aa5
Mariana <mariana.orozco@est.iudigital.edu.co>
2022-06-20 00:22:01 -0500

    Se añade archivo .env para configurar la uri de mongoDB, se crea archivo para realizar la conexión a mongoDB y se configura el llamado a la base de datos desde index.js

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+require("dotenv").config();
+const { getConnection } = require('./db/mongo');
+const PORT = process.env.PORT || 4000;
 const express = require("express");
 const app = express();
+
+// Conexión a MongoDB
+
+getConnection();
 
 // Configuraciones generales
 
 app.use(express.json());
 
 // Inicializando servidor
-app.listen(4000, () => {
-    console.log("Server started at port 4000");
+app.listen(PORT, () => {
+    console.log("Servidor inicializado en el puerto: ", PORT);
   });
